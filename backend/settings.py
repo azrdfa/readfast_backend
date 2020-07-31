e8a458908d2bc66d93ed27a9e8f76ab9206b31d3
Azhar Difa Arnanda <aazhard8@gmail.com>
2020-07-31 11:09:05 +0700

    create: deployment setting

--- backend/settings.py
+++ backend/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
 ]
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
